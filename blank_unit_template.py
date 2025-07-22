6528c0e81b8e062876c71f9b8370754644aea490
Wolf <d.wolftripp@gmail.com>
2025-07-22 02:54:27 -0700

    create unit template for easier unit creation

---
+++ blank_unit_template.py
@@ -0,0 +1,59 @@
+blank_unit_temp = {
+    "name": "",
+    "element": "",
+    "rarity": "",
+    "skills": [
+        {
+            "skill": 1,
+            "name": "",
+            "type": "",
+            "target": "",
+            "power": 0,
+            "effects": [
+                {
+                    "type": "",
+                    "target": "",
+                    "effect": "",
+                    "chance": 0,
+                    "value": 0,
+                    "duration": 0
+                }
+            ],
+            "description": ""
+        },
+        {
+            "skill": 2,
+            "name": "",
+            "power": 0,
+            "cool_down": 0,
+            "effects": [
+                {
+                    "type": "",
+                    "target": "",
+                    "effect": "",
+                    "chance": 0,
+                    "value": 0,
+                    "duration": 0
+                }
+            ],
+            "description": ""
+        },
+        {
+            "skill": 3,
+            "name": "",
+            "power": 0,
+            "cool_down": 0,
+            "effects": [
+                {
+                    "type": "",
+                    "target": "",
+                    "effect": "",
+                    "chance": 0,
+                    "value": 0,
+                    "duration": 0
+                }
+            ],
+            "description": ""
+        }
+    ]
+}
